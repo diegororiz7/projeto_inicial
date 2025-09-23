e7c1aed88c64ff279ef06fc467f76c1e5415b584
diegororiz7 <diegororiz7@gmail.com>
2025-09-23 19:53:01 -0300

    Aula 23/09/2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_final_fields, unused_field, unused_element, override_on_non_overriding_member, unused_local_variable
+// ignore_for_file: prefer_final_fields, unused_field, unused_element, override_on_non_overriding_member, unused_local_variable, unnecessary_const
 
 import 'package:flutter/material.dart';
 
@@ -49,7 +49,14 @@ class _TelaBaseState extends State<TelaBase> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(_titulos[_paginaAtual])),
+      appBar: AppBar(
+        title: Text(
+          _titulos[_paginaAtual],
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.indigo,
+        centerTitle: true,
+      ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -131,21 +138,144 @@ class TelaFilmes extends StatelessWidget {
 
     final filmesPorCategoria = {
       'Ação': [
-        {'titulo': 'Mad Max', 'ano': '2015', 'nota': '8.1'},
-        {'titulo': 'John Wick', 'ano': '2014', 'nota': '7.4'},
+        {
+          'titulo': 'Mad Max',
+          'ano': '2015',
+          'nota': '8.1',
+          'cartaz': 'src/mad_max.jpg',
+        },
+        {
+          'titulo': 'John Wick',
+          'ano': '2014',
+          'nota': '7.4',
+          'cartaz': 'src/john_wick.jpg',
+        },
       ],
       'Comédia': [
-        {'titulo': 'Se beber não case', 'ano': '2009', 'nota': '7.7'},
-        {'titulo': 'As branquelas', 'ano': '2004', 'nota': '5.7'},
+        {
+          'titulo': 'Se beber não case',
+          'ano': '2009',
+          'nota': '7.7',
+          'cartaz': 'src/se_beber.jpg',
+        },
+        {
+          'titulo': 'As branquelas',
+          'ano': '2004',
+          'nota': '5.7',
+          'cartaz': 'src/as_branquelas.jpg',
+        },
       ],
       'Drama': [
-        {'titulo': 'À procura da felicidade', 'ano': '2006', 'nota': '8.0'},
-        {'titulo': 'Clube da luta', 'ano': '1999', 'nota': '8.8'},
+        {
+          'titulo': 'À procura da felicidade',
+          'ano': '2006',
+          'nota': '8.0',
+          'cartaz': 'src/procura_felicidade.jpg',
+        },
+        {
+          'titulo': 'Clube da luta',
+          'ano': '1999',
+          'nota': '8.8',
+          'cartaz': 'src/clube_luta.jpg',
+        },
       ],
     };
 
     final filmes = filmesPorCategoria[categoriaArg] ?? [];
 
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Filmes de $categoriaArg',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+      ),
+      body: ListView.builder(
+        itemCount: filmes.length,
+        itemBuilder: (context, index) {
+          final filme = filmes[index];
+          return Card(
+            elevation: 3,
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            child: ListTile(
+              leading: const Icon(Icons.movie_creation, color: Colors.indigo),
+              title: Text(filme['titulo'] ?? ''),
+              subtitle: Text('Ano: ${filme['ano']} . Nota: ${filme['nota']}'),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                Navigator.pushNamed(context, '/detalhe', arguments: filme);
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class TelaDetalheFilme extends StatelessWidget {
+  final Map<String, String> filme;
+  const TelaDetalheFilme({super.key, required this.filme});
+
+  @override
+  Widget build(BuildContext context) {
+    final filmeArg = filme.isEmpty
+        ? ModalRoute.of(context)!.settings.arguments as Map<String, String>
+        : filme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          filmeArg['titulo'] ?? '',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                filmeArg['cartaz'] ?? '',
+                width: 300,
+                height: 450,
+                fit: BoxFit.contain,
+              ),
+              Text(
+                filmeArg['titulo'] ?? '',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5),
+              Text('Ano: ${filmeArg['ano']}', style: TextStyle(fontSize: 16)),
+              Text('Nota: ${filmeArg['nota']}', style: TextStyle(fontSize: 16)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TelaSobre extends StatelessWidget {
+  const TelaSobre({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          'Catálogo de Filmes\nVersão 1.0\nDesenvolvido com Flutter',
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
   }
 }
